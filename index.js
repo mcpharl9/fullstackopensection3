1ec0058b19b74d76334c0ffb5a3232db3285048c
Heather McPharlin <noncoffeedrinker@hotmail.com>
2021-04-14 14:00:19 -0400

    Add build

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('find', (request, response) => {
     return JSON.stringify({name: request.body.name, number: request.body.number})
 })
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :find'))
